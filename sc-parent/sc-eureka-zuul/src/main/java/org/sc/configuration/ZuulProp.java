7ccacb0d5874c4ac200e37e566853c901dece19a
lvjh <lvjh@gfresh.com>
2018-06-13 13:09:21 +0800

    1

--- sc-parent/sc-eureka-zuul/src/main/java/org/sc/configuration/ZuulProp.java
+++ sc-parent/sc-eureka-zuul/src/main/java/org/sc/configuration/ZuulProp.java
@@ -11,9 +11,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class ZuulProp {
 //    @Value("${zuulprop.loginAccessUrl}")
-    private String loginAccessUrl = "/login";
+    private String loginAccessUrl = "";
 //    @Value("${zuulprop.allowOrigin}")
-    private String allowOrigin = "172.16.4.61,localhost";
+    private String allowOrigin = ".gfresh.cn,//gfresh.cn,.igfresh.com";
 //    @Value("${zuulprop.oAuthTimes}")
     private Integer oAuthTimes = 5;
 //    @Value("${zuulprop.scheme}")
